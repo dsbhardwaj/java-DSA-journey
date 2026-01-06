3480db83bad20801c68180e947a58388344340ef
drishti <drishtiibhardwaj@gmail.com>
2026-01-06 17:59:35 +0530

    implemented all -ve numbers to left

---
+++ allNegativeNumberstoLeft.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class allNegativeNumberstoLeft{
+  public static void main(Strings[] args){
+    Scanner sc = new Scanner(System.in);
+    int arr[] = new int[n];
+    System.out.println("Enter the size of array:")
+    int n = sc.nextInt();
+    for(int i = 0 ; i<n ; i++){
+      
+    }
+
+  }
+}
