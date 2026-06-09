021232d24aece44e81035f36a876b1b1d1ef1f0a
drishti <drishtiibhardwaj@gmail.com>
2026-06-09 14:25:29 +0530

    methods

---
+++ Java/day-4/methods.java
@@ -0,0 +1,11 @@
+public class methods{
+    public static int square(int a){
+        return a*a;
+    }
+    public static void main(String[] args){
+        int x = square(4);
+
+
+        System.out.println(x);
+    }
+}
