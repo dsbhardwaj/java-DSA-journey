d01eed5e57b5bc5451bdb0131b98ec75e7816554
drishti <drishtiibhardwaj@gmail.com>
2026-06-25 16:27:26 +0530

    Sliding Window maximum sum of subarray

---
+++ Java/sliding-window/maxsumofsubarray.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class maxsumofsubarray{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of array:");
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    System.out.println("enter array elements:");
+    for(int i = 0 ; i < arr.length ; i++){
+        arr[i] = sc.nextInt();
+    }
+    System.out.println("enter the size of subarray");
+    int k = sc.nextInt();
+
+    int Windowsum = 0 ;
+    int maxSum = 0;
+    for(int i = 0 ; i < k ; i++){
+        Windowsum = Windowsum + arr[i];
+        maxSum = Windowsum;
+    }
+        for(int i = k ; i < arr.length ; i++){
+        Windowsum = Windowsum - arr[i -k] + arr[i];
+        if(Windowsum > maxSum){
+        maxSum = Windowsum;}
+    
+     maxSum = Math.max(maxSum , Windowsum);
+        }
+    System.out.println("The maximum sum of a subarray is:" + maxSum);
+    }
+}
