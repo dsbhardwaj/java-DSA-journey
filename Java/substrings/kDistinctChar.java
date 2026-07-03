0c41756e7f58c687a88edbdca0b3d03f1a1f782a
drishti <drishtiibhardwaj@gmail.com>
2026-07-03 23:57:06 +0530

    permutation of a string

---
+++ Java/substrings/kDistinctChar.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Scanner;
+public class kDistinctChar{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String:");
+        String s = sc.nextLine();
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+        int end = 0;
+        
+        for( end = 0 ; end < s.length ; end++)
+    }
+}
---
+++ Java/substrings/permutationString.java
@@ -0,0 +1,67 @@
+import java.util.HashMap;
+import java.util.Scanner;
+public class permutationString{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String 1 :");
+        String s1 = sc.nextLine();
+        System.out.println("Enter String 2 :");
+        String s2 = sc.nextLine();
+
+        HashMap<Character , Integer> map1 = new HashMap<>();
+
+        for(int i = 0 ; i < s1.length() ; i++){
+            if(map1.containsKey(s1.charAt(i)) == false ){
+                map1.put(s1.charAt(i), 1);
+            }else{
+                int freq = map1.get(s1.charAt(i));
+                freq = freq + 1 ;
+                map1.put(s1.charAt(i) , freq);
+
+            }           
+        }
+
+        HashMap<Character , Integer > map2 = new HashMap<>();
+        int end = 0;
+        int Start = 0 ; 
+        for( end = 0 ; end < s1.length() ; end++){
+            if(map2.containsKey(s2.charAt(end)) == false){
+                map2.put(s2.charAt(end), 1);
+            }else{
+                int freq2 = map2.get(s2.charAt(end));
+                freq2 = freq2 + 1 ;
+                map2.put(s2.charAt(end), freq2);
+            }
+            if(map1.equals(map2)){
+                System.out.println("True");
+                break;
+            }
+        }
+            for(end = s1.length() ; end < s2.length() ; end++){
+                if(map2.containsKey(s2.charAt(end)) == false){
+                    map2.put(s2.charAt(end), 1);
+                }else{
+                    int f = map2.get(s2.charAt(end));
+                    f = f + 1;
+                    map2.put(s2.charAt(end), f);
+                }
+            
+            if(map2.get(s2.charAt(Start)) > 1){
+                int frequency = map2.get(s2.charAt(Start));
+                frequency = frequency -1 ;
+                map2.put(s2.charAt(Start) ,  frequency);
+            }else{
+                map2.remove(s2.charAt(Start));
+                
+                }
+                Start++;
+
+                if(map1.equals(map2)){
+                    System.out.println("True");
+                    break;
+                }
+            }
+        }
+    }       
+ 
