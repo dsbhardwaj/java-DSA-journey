de7d4cb80b7e1874af7a524bf7c47fca0b5da4a0
drishti <drishtiibhardwaj@gmail.com>
2026-07-27 10:49:36 +0530

    Matrix

---
+++ Java/Matrices/matrix.java
@@ -0,0 +1,21 @@
+import java.util.Scanner ;
+public class matrix{
+    public static void main(String[]  args){
+        Scanner sc = new Scanner(System.in);
+    
+        int[][] matrix = new int[3][3];
+         for(int i = 0 ; i < matrix.length ; i++ ){
+            for(int j = 0 ; j < matrix[i].length ; j++){
+                matrix[i][j] = sc.nextInt();   
+            }
+         }
+         for(int i = 0 ; i < matrix.length ; i++){
+            for(int j = 0 ; j < matrix[i].length ; j++){
+                System.out.print(matrix[i][j] ); 
+            }
+            System.out.println();
+         }
+
+         sc.close();
+    }
+}
