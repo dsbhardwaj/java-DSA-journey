551d0836142baf2d131d91a41daa43e61fab0c18
drishti <drishtiibhardwaj@gmail.com>
2026-07-29 23:48:32 +0530

    sum rows

---
+++ Java/Matrices/sumRows.java
@@ -0,0 +1,13 @@
+public class sumRows{
+    public void sum(int[][] matrix ){
+        for(int i = 0 ; i < matrix.length ; i++){
+            int sum = 0 ;
+            for(int j = 0 ; j < matrix[i].length ; j++){
+                sum += matrix[i][j];
+            }
+                System.out.print(sum);
+            }
+        
+    
+    }
+}
