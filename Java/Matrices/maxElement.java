d9c8ef7cb3ecb3f83bd6c6b63f4c193bea6504c9
drishti <drishtiibhardwaj@gmail.com>
2026-07-29 23:59:11 +0530

    max element

---
+++ Java/Matrices/maxElement.java
@@ -0,0 +1,13 @@
+public class maxElement{
+    public int max(int[][] matrix){
+        int max = Integer.MIN_VALUE ;
+        for(int i = 0 ; i < matrix.length ; i++){
+            for(int j = 0 ; j < matrix[i].length ; j++){
+                if(matrix[i][j] > max){
+                    max = matrix[i][j];
+                }
+            }
+        }
+        return max ;
+    }
+}
