4677bba0ec83887a14b335109557152712d04fce
drishti <drishtiibhardwaj@gmail.com>
2026-07-30 17:46:05 +0530

    diagonal Matrix

---
+++ Java/Matrices/DiagonalEnhanced.java
@@ -0,0 +1,9 @@
+public class DiagonalEnhanced{
+    public void diagonalELements(int[][] matrix){
+
+        for(int i = 0 ; i< matrix.length ; i++)
+                { int j = i ;
+                    System.out.println(matrix[i][j]);
+                }
+            }
+        }
