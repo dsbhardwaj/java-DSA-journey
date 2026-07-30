b2f2ca7252c0125ff22d266b12b8c59934e726de
drishti <drishtiibhardwaj@gmail.com>
2026-07-30 17:41:29 +0530

    diagonal elements

---
+++ Java/Matrices/primaryDiagonal.java
@@ -0,0 +1,13 @@
+public class primaryDiagonal{
+    public void diagonalELements(int[][] matrix){
+
+        for(int i = 0 ; i< matrix.length ; i++){
+            for(int j = 0 ; j < matrix[i].length ; j++){
+                if(i == j){
+                    System.out.println(matrix[i][j]);
+                }
+            }
+        }
+
+    }
+}
