6b6d272dce2cee77db3cde91eb91dd01d07a7a05
drishti <drishtiibhardwaj@gmail.com>
2026-08-05 13:37:59 +0530

    snake traversal

--- Java/Matrices/snakePattern.java
+++ Java/Matrices/snakePattern.java
@@ -6,10 +6,10 @@ public class snakePattern{
 
                 for(int i = 0 ; i < matrix.length ; i++){
                     if(i%2 == 0){
-                        for(int j= 0 ; j < matrix[i].length ; i++){
+                        for(int j= 0 ; j < matrix[i].length ; j++){
                             ans.add(matrix[i][j]);
                     }
-                }else{for(int j = matrix[0].length ; j >= 0 ; j--){
+                }else{for(int j = matrix[0].length -1 ; j >= 0 ; j--){
                         ans.add(matrix[i][j]);
                     }
                 }
