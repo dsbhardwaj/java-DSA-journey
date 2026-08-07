9ec96ab8a84761a6f72bf3e88604f887b8db73e6
drishti <drishtiibhardwaj@gmail.com>
2026-08-07 22:39:32 +0530

    matrixxx

--- Java/Matrices/addition.java
+++ Java/Matrices/addition.java
@@ -1,5 +1,19 @@
-public class addition{
-    public void matrixAdd(int[][] m1 , int[][] m2 ){
-        
+public class addition {
+
+    public int[][] add(int[][] A, int[][] B) {
+
+        if (A.length != B.length || A[0].length != B[0].length) {
+            throw new IllegalArgumentException("Matrices must have the same dimensions.");
+        }
+
+        int[][] result = new int[A.length][A[0].length];
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                result[i][j] = A[i][j] + B[i][j];
+            }
+        }
+
+        return result;
     }
 }
--- Java/Matrices/saddlePoint.java
+++ Java/Matrices/saddlePoint.java
@@ -1,28 +1,36 @@
-public class saddlePoint{
-    public void isSaddle(int[][] matrix){
-            int mincolm = 0 ;
-        for(int i = 0 ; i < matrix.length ; i++){
-            for(int j = 0 ; j < matrix[i].length ; j++){
-                  int min = matrix[0][0];
-                if(matrix[i][j] < min){
+public class saddlePoint {
+
+    public void findSaddlePoint(int[][] matrix) {
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            int min = matrix[i][0];
+            int minCol = 0;
+
+            // Find the minimum element in the current row
+            for (int j = 1; j < matrix[i].length; j++) {
+                if (matrix[i][j] < min) {
                     min = matrix[i][j];
-                    mincolm = j ;
-                }
-                    int max = matrix[0][0];
-
-                    for(int n = j ; n < matrix[0].length ; n++){
-                        for(int m = 0 ; m < matrix.length ; m++){
-                            if(matrix[m][n] > max){
-                                max = matrix[m][n];
-                            }
-
-                        }
-                    }
-
-                    if(min == max){
-                        System.out.println( matrix[i][j]);
-                    }
+                    minCol = j;
                 }
             }
+
+            // Check whether it is the maximum in its column
+            boolean isSaddle = true;
+
+            for (int k = 0; k < matrix.length; k++) {
+                if (matrix[k][minCol] > min) {
+                    isSaddle = false;
+                    break;
+                }
+            }
+
+            if (isSaddle) {
+                System.out.println("Saddle Point: " + min);
+                return;
+            }
         }
+
+        System.out.println("No Saddle Point Found");
     }
+}
